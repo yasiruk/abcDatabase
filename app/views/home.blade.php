dcd3f6a120ba12223c4b1b6bf8111ed130afc2ba
Yasiru Kassapa <wik2kassa@gmail.com>
2014-12-02 18:26:34 +0530

    Created home route and added more to the layout

---
+++ app/views/home.blade.php
@@ -0,0 +1,5 @@
+@extends('layout')
+
+@section('content')
+<h2> This is the unupdated home page</h2>
+@stop
--- app/views/layout.blade.php
+++ app/views/layout.blade.php
@@ -1,5 +1,6 @@
 <html>
     <body>
+    	
         <h1>Page Skeliton is not yet updated</h1>
 
         @yield('content')
