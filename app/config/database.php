e719c62a92fdaed81dafec2d988b47ad7f4dca8c
Yasiru Kassapa <wik2kassa@gmail.com>
2014-12-02 19:28:41 +0530

    Created migrations for users table

--- app/config/database.php
+++ app/config/database.php
@@ -55,8 +55,8 @@ return array(
 		'mysql' => array(
 			'driver'    => 'mysql',
 			'host'      => 'localhost',
-			'database'  => 'forge',
-			'username'  => 'forge',
+			'database'  => 'oosd',
+			'username'  => '',
 			'password'  => '',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
---
+++ app/database/migrations/2014_12_02_130832_create_user_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUserTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('users', function($table) {
+			$table->increments('id');
+			$table->string('username')->unique();
+			$table->string('first_name');
+			$table->string('last_name');
+			$table->string('passoword');
+			$table->index('username');
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('users');
+	}
+
+}
